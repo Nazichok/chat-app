68bc69a3cf8a9858bb6efa1d379a9b69f6a7b9b0
Nazar Dizhak <nazar.dizhak@sombrainc.com>
2024-10-08 23:05:55 +0300

    Fix full height on mobile device

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+const resizeOps = () => {
+  document.documentElement.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
+};
+
+resizeOps();
+window.addEventListener("resize", resizeOps);
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
