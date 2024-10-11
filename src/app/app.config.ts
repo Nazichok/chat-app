0108ed9c1a6b0befafac1c3a15d8f78259082ee2
Nazar Dizhak <nazar.dizhak@sombrainc.com>
2024-10-11 19:58:20 +0300

    disable service-worker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,27 +3,37 @@ import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptors,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { authInterceptor } from './helpers/auth.interceptor';
 import { errorInterceptor } from './helpers/error.interceptor';
 import { MessageService } from 'primeng/api';
 import { CancelSameApisInterceptor } from './helpers/cancel-same-request.interceptor';
 import { DialogService } from 'primeng/dynamicdialog';
-import { provideServiceWorker } from '@angular/service-worker';
+// import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(
       withInterceptorsFromDi(),
-      withInterceptors([loadingInterceptor, errorInterceptor, authInterceptor])
+      withInterceptors([loadingInterceptor, errorInterceptor, authInterceptor]),
     ),
-    { provide: HTTP_INTERCEPTORS, useClass: CancelSameApisInterceptor, multi: true },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CancelSameApisInterceptor,
+      multi: true,
+    },
     provideAnimations(),
     MessageService,
-    DialogService, provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })
+    DialogService,
+    // provideServiceWorker('ngsw-worker.js', {
+    //   enabled: !isDevMode(),
+    //   registrationStrategy: 'registerWhenStable:30000',
+    // }),
   ],
 };
