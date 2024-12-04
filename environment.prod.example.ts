10515ff0d2eb3051e4aae56b875cb8db38389607
Nazar Dizhak <nazar.dizhak@sombrainc.com>
2024-12-04 02:58:50 +0200

    Add Dockerfile; Add nginx.conf; Add environment.prod example

---
+++ environment.prod.example.ts
@@ -0,0 +1,8 @@
+// environment.prod is here because render.com lets use env files only on top level
+export const environment = {
+  production: true,
+  serverUrl: 'http://localhost:8080',
+  localCryptoKey: 'somelocalkey',
+  googleClientId: 'somegoogleclientid.apps.googleusercontent.com',
+  vapidPublicKey: 'somekey'
+};
